02c916a79171abfc2b0570b44a0aba08f28ac615
Daniel Jacob <danieljaroszczuk28@gmail.com>
2024-06-23 20:15:03 -0300

    Adição das funções na tela admin

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,35 +1,20 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    for(let i of document.querySelectorAll(".btn_remove_user")){
-        i.addEventListener('click',function(){removeButtonClick(this.parentElement)});
-    }
+    document.getElementById("btn_delete_all").addEventListener("click",function(){removeAllButtonClick()});
+    document.getElementById("btn_limpar_campos" ).addEventListener("click",function(){cleanFieldsButtonClick()});
+    document.getElementById("btn_add_user").addEventListener("click",function(){addUserButtonClick()});
+    document.getElementById("input_search_user").addEventListener("input",function(){searchInputType(this.value)});
+    
+    refreshPageUsersList(getAllUsers());
 
-    /*
-    TESTE
-    window.localStorage.clear();
-    let teste1 = {
-        email: "exemplo@teste",
-        nome: "daniel"
-    };
-    let teste2 = {
-        email: "exemplo@teste2",
-        nome: "felipe"
-    };
-    let teste3 = {
-        email: "exemplo@teste2",
-        nome: "daniela"
+    function searchInputType(text){
+        if(text.trim() == ""){
+            refreshPageUsersList(getAllUsers());
+        }
+        let users = getAllUsers();
+        filteredUsers = users.filter(x=> (x.nome.trim().toUpperCase().indexOf(text.trim().toUpperCase()) != -1 || x.email.trim().toUpperCase().indexOf(text.trim().toUpperCase()) != -1));
+        refreshPageUsersList(filteredUsers);
     }
-    let teste4 = {
-        email: "exemplo@teste2",
-        nome: "pedro"
-    };
-    window.localStorage.setItem("1", JSON.stringify(teste1));
-    window.localStorage.setItem("2", JSON.stringify(teste2));
-    window.localStorage.setItem("3", JSON.stringify(teste3));
-    window.localStorage.setItem("6", JSON.stringify(teste4));
-    window.localStorage.setItem("lastId", "6");
-
-    console.log(getAllUsers());*/
 
     function uptadeLastId(){
         let id = parseInt(JSON.parse(localStorage.getItem("lastId")));
@@ -37,39 +22,153 @@ document.addEventListener("DOMContentLoaded", function (event) {
         localStorage.setItem("lastId", (id).toString());
     };
     function getLastId() {
+        let idText = localStorage.getItem("lastId");
+        if (idText == null) {
+            window.localStorage.setItem("lastId", "0");
+            return 0;
+        }
         let id = parseInt(JSON.parse(localStorage.getItem("lastId")));
         return id;
     };
     function addUser(objUser) {
-        let json = JSON.stringify(objUser);
-        localStorage.setItem(getLastId() + 1, json);
-        uptadeLastId();
+        let users = getAllUsers();
+        let usersFilter = users.filter(x=> (x.nome.trim().toUpperCase() == objUser.nome.trim().toUpperCase() && x.email.trim().toUpperCase() == objUser.email.trim().toUpperCase()));
+        if(usersFilter.length > 0){
+            alert("Usuário já cadastrado");
+            cleanFieldsButtonClick();
+        }else{
+            users.push(objUser);
+            localStorage.setItem("users",JSON.stringify(users));
+            uptadeLastId();
+        }
+
     };
     function removeUser(id) {
-        localStorage.removeItem(String(id));
+        let users = getAllUsers();
+        localStorage.setItem("users",JSON.stringify(users.filter(x=>x.id.trim().toUpperCase() != id)));
     };
-    function getUserById(id) {
-        let obj = JSON.parse(localStorage.getItem(String(id)));
-        return obj;
-    };
+
     function getAllUsers() {
+        let users = JSON.parse(localStorage.getItem("users"));
+        if(users == null || users == undefined){
+            return [];
+        }
+        return users;
+
+        /*
         let users = [];
         let keys = Object.keys(localStorage).filter(x => x != "lastId");
         for (let key of keys) {
             let obj = localStorage.getItem(key);
             users.push(JSON.parse(obj));
         }
-        return users;
+        return users;*/
     };
     function removeAllUsers(){
         localStorage.clear();
     }
 
+    //delete user button click "X"
     function removeButtonClick(element){
-        console.log(element);
-        console.log("TESTE");
+        console.log("ui");
+        let id = element.getElementsByClassName("id_information")[0].innerHTML;
+        removeUser(id);
+        refreshPageUsersList(getAllUsers());
     }
 
-    function refreshList(){}
+    function removeAllButtonClick(){
+        removeAllUsers();
+        refreshPageUsersList(getAllUsers());
+    }
+
+    function cleanFieldsButtonClick(){
+        document.getElementById("nome").value = "";
+        document.getElementById("email").value = "";
+    }
+
+    function getCurrentDate(){
+        let date = new Date();
+        let dia = (date.getDate().toString().padStart(2,"0"));
+        let mes = ((date.getMonth() + 1).toString().padStart(2,"0"));
+        let ano = date.getFullYear().toString();
+        return `${dia}/${mes}/${ano}`;
+    }
+
+
+    function addUserButtonClick() {
+        console.log("entrou aqui");
+        //PEGA O NOME
+        let inputNome = document.getElementById("nome").value;
+        //PEGA O EMAIL
+        let inputEmail = document.getElementById("email").value;
+        if(inputNome.trim() == "" || inputEmail.trim() == ""){
+            return "";
+        }
+        let user = {
+            data: getCurrentDate(),
+            id: (getLastId()+1).toString(),
+            email: inputEmail,
+            nome: inputNome
+        };
+        addUser(user);
+        refreshPageUsersList(getAllUsers());
+    }
+
+    function cleanPageUsersList(){
+        let lis = document.getElementsByClassName("users_list_item");
+        for(let i = lis.length-1 ; i >= 0 ; i--){
+            lis[i].parentNode.removeChild(lis[i]);
+        }
+    }
+
+
+    function refreshPageUsersList(users) {
+        cleanPageUsersList();
+        for (let user of users) {
+            let li = document.createElement("li");
+            li.className = "users_list_item";
+
+            //ID
+            let spanId = document.createElement("SPAN");
+            spanId.className = "id_information";
+            let id = document.createTextNode(user.id);
+            spanId.appendChild(id);
+            li.appendChild(spanId);
+
+            //DATA
+            let spanData = document.createElement("SPAN");
+            spanData.className = "list_information";
+            let date = document.createTextNode(user.data);
+            spanData.appendChild(date);
+            li.appendChild(spanData);
+
+            //NOME
+            let spanNome = document.createElement("SPAN");
+            spanNome.className = "list_information";
+            let nome = document.createTextNode(user.nome);
+            spanNome.appendChild(nome);
+            li.appendChild(spanNome);
+            //EMAIL
+            let spanEmail = document.createElement("SPAN");
+            spanEmail.className = "list_information";
+            let email = document.createTextNode(user.email);
+            spanEmail.appendChild(email);
+            li.appendChild(spanEmail);
+
+            //X
+            let spanX = document.createElement("SPAN");
+            spanX.className = "btn_remove_user";
+            let x = document.createTextNode("\u00D7");
+            spanX.appendChild(x);
+            li.appendChild(spanX);
+
+            document.getElementById("users_list").appendChild(li);
+        }
+
+        for(let i of document.querySelectorAll(".btn_remove_user")){
+            i.addEventListener('click',function(){removeButtonClick(this.parentElement)});
+        }
+
+    }
 
 })
